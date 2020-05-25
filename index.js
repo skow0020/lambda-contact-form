1bffe20a216e6fdd46f92c1925624198d784dc20
Colin <skow0020@gmail.com>
2020-05-25 16:49:49 -0500

    Update stuff for node

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-const PORT=8080; 
+// const PORT=8080; 
 
-fs.readFile('./contact-form-lambda.html', function (err, html) {
+// fs.readFile('./contact-form-lambda.html', function (err, html) {
 
-    if (err) throw err;    
+//     if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(PORT);
+// });
+
+var express = require('express')
+var app = express()
+
+app.use(express.static('www'));
+
+var server = app.listen(8000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Express app listening at http://%s:%s', host, port)
+
+})
