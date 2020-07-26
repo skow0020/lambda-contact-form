6604530a11212d4784b9cdc815f971087c54d00e
Colin SKow <skow0020@gmail.com>
2020-07-26 16:17:22 -0500

    Add puppeteer test

--- test/spec.js
+++ test/spec.js
@@ -26,9 +26,29 @@ describe('loading express', function () {
   it('Renders Contact pg html', function testPath(done) {
     request(server)
       .get('/')
-      .expect(function(res) {
+      .expect(function (res) {
         expect(res.text).toContain('<div class="container-contact3">')
       })
       .expect(200, done);
+  });
+});
+
+describe('Contact Page UI', () => {
+  beforeAll(async () => {
+    await page.goto('https://skow-contact-form-dev.herokuapp.com/');
+  });
+
+  it('should be able to contact via UI', async () => {
+    await expect(page.title()).resolves.toMatch('Contact');
+    
+    await page.$eval('#name', el => el.value = 'JOHN SILYRI');
+    await page.$eval('#email', el => el.value = 'test@example.com');
+    await page.$eval('#message', el => el.value = 'Example message');
+
+    await page.click(".contact3-form-btn");
+
+    await page.waitForFunction(
+      'document.querySelector("#alert").innerText.includes("Thanks for contacting us.")'
+    );
   });
 });
